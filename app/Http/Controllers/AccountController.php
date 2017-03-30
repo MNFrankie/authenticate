c19f3409cbbe2631478d75e89e17df2ddfe32114
MNFrankie <sicnare@gmail.com>
2017-03-30 16:15:03 +0300

    Account dynamics

---
+++ app/Http/Controllers/AccountController.php
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+
+public function postSignIn() {
+	$validator = Validator::make(Input::all()
+	array(
+			'email' => 'required|email',
+			'password' => 'required'
+		)
+	),
+	if($validator->fails()){
+		return Redirect::route('account-sign-in')
+				->withErrors($validator)
+				->withInput();
+	} else{
+		$auth = Auth::attempt(array(
+			'email' => Input::get('email'),
+			'password' => Input::get('password'),
+			'active' => 1
+		));
+
+		if($auth) {
+			//redirects to the intended page
+			return Redirect::intended('/');
+
+		} else {
+			return Redirect::route('account-sign-in')
+					->with('global', 'Email/password wrong, or account not activated.')
+		}
+	}
+}
